d09e5d7d537d7cf719cd9f839466234c364593ee
JordParma <johnnyfroehlich@gmail.com>
2017-06-02 22:29:11 -0700

    Data processing

--- Performance/Data/Processing/Dp_v3.py
+++ Performance/Data/Processing/Dp_v3.py
@@ -1,11 +1,10 @@
 import numpy as np
 import plotly.plotly as py
 import plotly.graph_objs as go
-
+from matplotlib.ticker import AutoMinorLocator, MultipleLocator, FuncFormatter
 import matplotlib.pyplot as plt
 from matplotlib import pylab
 from scipy.interpolate import UnivariateSpline
-
 import csv
 
 def readMyFile(filename):
@@ -28,7 +27,7 @@ head_s, flow_s, flow, head = readMyFile("17500,csv")
 head = np.delete(head, np.where(head == '')[0])
 flow = np.delete(flow, np.where(flow == '')[0])
 
-#flow_adj = flow[1:-(len(flow)-len(head))]
+# Remove point beyond run-out
 flow_adj = (flow[1:])
 head_adj = (head[1:])
 
@@ -38,14 +37,31 @@ print(len(flow_adj), len(head_adj))
 
 # Trend lines
 spl = UnivariateSpline(sorted(flow_adj), sorted(head_adj, reverse = True))
-xs = np.linspace(3, 9.5, 1000)
-spl.set_smoothing_factor(1)
-#plt.plot(xs, spl(xs), 'g', lw=2)
+xs = np.linspace(3.5, 9.5, 1000)
+spl.set_smoothing_factor(100)
 
 # Plot Main
-#plt.plot(flow_adj, head_adj, 'r--', flow_s, head_s, 'r--', ms=5)
+# plt.plot(flow_adj, head_adj, 'r--', flow_s, head_s, 'r--', ms=5)
+plt.figure(figsize=(10, 6), dpi=80)
+plt.plot(xs, spl(xs), 'k--', lw=2.5)
+plt.plot(flow_s, head_s, 'k-', lw=2.5, label = 'System')
 
-plt.plot(xs, spl(xs), 'g', flow_s, head_s, 'r--', ms=5)
+#################################################################################
+
+# Annotations
+# plt.plot([8.96, 8.96], [0, 449.438], color='red', linewidth=2.5, linestyle="--")
+plt.scatter([8.9612, ], 449.438, color='red', s = 100)
+plt.annotate('BEP',
+             xy=(8.96, 449.438), xycoords='data',
+             xytext=(-80, 90), textcoords='offset points', fontsize=16,
+             arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=.1"))
+plt.legend(loc='upper left')
+
+plt.xlabel('Capacity [G.P.M.]', fontsize=16)
+plt.ylabel('Head [ft]', fontsize=16)
+
+
+#################################################################################
 
 plt.axis([0, 12, 0, 1000])
 plt.show()
